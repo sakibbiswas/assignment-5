ec710b781c22624c3ab01d298b5fba433a01418e
sakibbiswas <sakibsakib99880@gmail.com>
2023-02-18 18:18:57 +0600

    Rhombus added

--- js/calculate.js
+++ js/calculate.js
@@ -40,8 +40,27 @@ document.getElementById('rectangle-calc').addEventListener('click', function () 
 // Parallelogram js
 document.getElementById('Parallelogram-calc').addEventListener('click', function () {
     const ParallelogramArea = 10 * 12;
-    const Parallelogram  =getTextvalueById('total-Parallelogram')
-    setElemenTextById('total-Parallelogram',ParallelogramArea)
+    const Parallelogram = getTextvalueById('total-Parallelogram')
+    setElemenTextById('total-Parallelogram', ParallelogramArea)
+});
+// Rhombus js
+document.getElementById('Rhombus-calc').addEventListener('click',function () {
+    const RhombusArea = 0.5 * 16 * 8;
+    const Rhombus = getTextvalueById('total-Rhombus')
+    setElemenTextById('total-Rhombus',RhombusArea)
 })
+
+// Pentagon js
+// document.getElementById('Pentagon-calc').addEventListener('click', function () {
+//     const PentagonArea = 0.5 * 6 * 10;
+//     const Pentagon = getTextvalueById('total-Pentagon')
+//     setElemenTextById('total-Pentagon', PentagonArea)
+
+// Ellipse js
+// document.getElementById('Ellipse-calc'.addEventListener('click', function () {
+//     const EllipseArea = 3.1416 * 10 * 4;
+//     const Ellipse = getTextvalueById('total-Ellipse')
+//     setElemenTextById('total-Ellipse')
+// }))
 
 
